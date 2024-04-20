d28c65ed8b50a7a27cc51ff8da63111072560b76
meerkat.log <67777655+gwihyeon@users.noreply.github.com>
2024-04-20 23:59:50 +0900

    Update interface-list

--- interface-list/libpcap_build.sh
+++ interface-list/libpcap_build.sh
@@ -4,6 +4,8 @@ target_name="$1"
 
 wget https://www.tcpdump.org/release/libpcap-1.10.4.tar.xz
 tar -xf libpcap-1.10.4.tar.xz
+rm libpcap-1.10.4.tar.xz
+
 cd libpcap-1.10.4
 mkdir build
 cd build
@@ -17,4 +19,3 @@ cmake -DCMAKE_TOOLCHAIN_FILE=../../toolchain/"$target_name".cmake \
 make
 make install
 
-rm libpcap-1.10.4.tar.xz
